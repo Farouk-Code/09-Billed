976e723483a6e96d6858118421fb955ef5e6e43b
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-06-20 20:07:12 +0200

    Correction bug 03 - Upload d'images uniquement pour les notes de frais côté employés - fini

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -46,6 +46,29 @@ export default class NewBill {
 
       return;
     }
+
+    const filePath = e.target.value.split(/\\/g);
+    const fileName = filePath[filePath.length - 1];
+    const formData = new FormData();
+    const email = JSON.parse(localStorage.getItem("user")).email;
+    formData.append("file", file);
+    formData.append("email", email);
+
+    this.store
+      .bills()
+      .create({
+        data: formData,
+        headers: {
+          noContentType: true,
+        },
+      })
+      .then(({ fileUrl, key }) => {
+        console.log(fileUrl);
+        this.billId = key;
+        this.fileUrl = fileUrl;
+        this.fileName = fileName;
+      })
+      .catch((error) => console.error(error));
   };
   handleSubmit = (e) => {
     e.preventDefault();
